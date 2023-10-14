2d9272950c70843ee94c7edcfaddf29d1d2a82bc
Yao Xiao <yxiao977@endeavour2.hpc.usc.edu>
2023-10-13 17:20:57 -0700

    reformatted

--- data_util.py
+++ data_util.py
@@ -8,8 +8,9 @@ from inspect import signature
 
 
 import logging
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
-logger = logging.getLogger('data_util')
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
+logger = logging.getLogger("data_util")
 logger.setLevel(logging.DEBUG)
 logger.propagate = False  # Disable propagation to the root logger
 
@@ -18,17 +19,19 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+
 def add_partial_thres_arguments(func):
     def wrapper(*args, **kwargs):
         if len(args) + len(kwargs) == 1:
             if len(args) == 1:
                 thres_value = args[0]
             else:
-                thres_value = kwargs['thres']
+                thres_value = kwargs["thres"]
 
             return functools.partial(func, thres=thres_value)
         else:
             return func(*args, **kwargs)
+
     return wrapper
 
 
@@ -55,6 +58,7 @@ def cis_total_ratio_filter(clr, thres=0.5):
 
     return bin_mask
 
+
 def generate_bin_mask(clr, filters=[None]):
     """
     Generates a binary mask for a given `clr` object based on a list of filters and thresholds.
@@ -72,13 +76,14 @@ def generate_bin_mask(clr, filters=[None]):
         A binary mask indicating which genomic bins pass all filters.
     """
     if not isinstance(filters, list):
-        logger.error('filter_lst parameter takes a list')
-        
+        logger.error("filter_lst parameter takes a list")
+
     bin_mask = np.array([True] * clr.bins().shape[0])
     for filter in filters:
         bin_mask *= filter(clr)
-    
+
     return bin_mask
+
 
 def _pixel_filter(chunk_pixels, good_bins_index):
     """
@@ -96,9 +101,12 @@ def _pixel_filter(chunk_pixels, good_bins_index):
     pandas.DataFrame
         A DataFrame containing only the pixels whose bin1_id and bin2_id are in good_bins_index.
     """
-    
-    pixels_mask = chunk_pixels['bin1_id'].isin(good_bins_index) * chunk_pixels['bin2_id'].isin(good_bins_index)
+
+    pixels_mask = chunk_pixels["bin1_id"].isin(good_bins_index) * chunk_pixels[
+        "bin2_id"
+    ].isin(good_bins_index)
     return chunk_pixels[pixels_mask]
+
 
 def pixel_iter_chunks(clr, chunksize):
     """
@@ -121,23 +129,26 @@ def pixel_iter_chunks(clr, chunksize):
         chunk = selector[lo:hi]
         yield chunk
 
-def pool_decorator(func):
 
+def pool_decorator(func):
     def wrapper(*args, **kwargs):
         pool = None
-        if 'map' not in kwargs.keys() and len(args) <= len(signature(func).parameters) - 1:
-            if 'nproc' in kwargs.keys():
-                if kwargs['nproc'] <= 1:
+        if (
+            "map" not in kwargs.keys()
+            and len(args) <= len(signature(func).parameters) - 1
+        ):
+            if "nproc" in kwargs.keys():
+                if kwargs["nproc"] <= 1:
                     mymap = map
                 else:
-                    logger.debug('Start to use pool')
-                    pool = Pool(kwargs['nproc'])
+                    logger.debug("Start to use pool")
+                    pool = Pool(kwargs["nproc"])
                     mymap = pool.map
             elif len(args) == len(signature(func).parameters) - 1:
                 if args[len(signature(func).parameters) - 2] <= 1:
                     mymap = map
                 else:
-                    logger.debug('Start to use pool')
+                    logger.debug("Start to use pool")
                     pool = Pool(args[len(signature(func).parameters) - 2])
                     mymap = pool.map
             else:
@@ -146,16 +157,18 @@ def pool_decorator(func):
             func(*args, **kwargs, map=mymap)
 
         else:
-
             func(*args, **kwargs)
-        
-        if pool != None: 
+
+        if pool != None:
             pool.close()
 
     return wrapper
 
+
 @pool_decorator
-def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, nproc=1, map=map):
+def create_filtered_cooler(
+    output_uri, clr, bin_mask, chunksize=10_000_000, nproc=1, map=map
+):
     """
     Create a filtered cooler file from a given cooler object and a binary mask of good bins.
 
@@ -178,15 +191,20 @@ def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, npro
     None
     """
     if len(bin_mask) != clr.bins().shape[0]:
-        raise ValueError('bin_mask should have the same length as bin table in cool file')
-    logger.debug('Start to create cooler file...')
+        raise ValueError(
+            "bin_mask should have the same length as bin table in cool file"
+        )
+    logger.debug("Start to create cooler file...")
     bin_table = clr.bins()[:]
     good_bins_index = np.array(range(clr.bins().shape[0]))[bin_mask]
     pixels_filter = functools.partial(_pixel_filter, good_bins_index=good_bins_index)
 
-    cooler.create_cooler(output_uri, bins=bin_table, pixels=map(pixels_filter, pixel_iter_chunks(clr, chunksize)), ordered=True, columns=['count'])
+    cooler.create_cooler(
+        output_uri,
+        bins=bin_table,
+        pixels=map(pixels_filter, pixel_iter_chunks(clr, chunksize)),
+        ordered=True,
+        columns=["count"],
+    )
 
-    logger.debug('done')
-
-
-# use the black
+    logger.debug("done")
--- test_func.py
+++ test_func.py
@@ -1,5 +1,6 @@
-import os 
+import os
 import sys
+
 cwd = os.getcwd()
 sys.path.append(cwd)
 import data_util
@@ -9,8 +10,8 @@ import pandas as pd
 import cooltools
 import numpy as np
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
-logger = logging.getLogger('test_func')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
+logger = logging.getLogger("test_func")
 logger.setLevel(logging.DEBUG)
 logger.propagate = False  # Disable propagation to the root logger
 
@@ -21,19 +22,14 @@ logger.addHandler(ch)
 # logger.disabled = True
 
 
-
 # create a small toy example inside the test func with a human readable small data
 def test_cis_total_filter(thres, clr_test=None):
-
     if clr_test == None:
-        logger.debug('Start to generate a toy example...')
+        logger.debug("Start to generate a toy example...")
         # Generate a toy example
-        bins=pd.DataFrame(
-            [["chr1", 0, 5],
-                ["chr1", 5, 8],
-                ["chrX", 0, 5],
-                ["chrX", 5, 10]],
-        columns=["chrom", "start", "end"],
+        bins = pd.DataFrame(
+            [["chr1", 0, 5], ["chr1", 5, 8], ["chrX", 0, 5], ["chrX", 5, 10]],
+            columns=["chrom", "start", "end"],
         )
 
         # only has contacts within the same chromsomes
@@ -41,29 +37,31 @@ def test_cis_total_filter(thres, clr_test=None):
         # > 0.5 cis-total ratio
         # = 0.5 cis-total ratio
         pixels = pd.DataFrame(
-            [[0, 0, 6], 
-            [1, 2, 3], [1, 3, 35],
-            [2, 2, 3], [2, 3, 30],
-            [3, 3, 5]], 
-            columns=["bin1_id", "bin2_id", "count"]
+            [[0, 0, 6], [1, 2, 3], [1, 3, 35], [2, 2, 3], [2, 3, 30], [3, 3, 5]],
+            columns=["bin1_id", "bin2_id", "count"],
         )
 
     clr_file = "/home1/yxiao977/sc1/train_akita/test_data/test_data_util.cool"
     cooler.create_cooler(clr_file, bins, pixels)
     clr_test = cooler.Cooler(clr_file)
-    cooler.balance_cooler(clr_test, ignore_diags=0, store=True, store_name='weight')
+    cooler.balance_cooler(clr_test, ignore_diags=0, store=True, store_name="weight")
     clr_test = cooler.Cooler(clr_file)
 
+    logger.debug("Start to test filter_pixels function...")
+    output_path = (
+        f"/home1/yxiao977/sc1/train_akita/test_data/test_data_util_{thres}filtered.cool"
+    )
+    bin_mask = data_util.generate_bin_mask(
+        clr_test, [data_util.cis_total_ratio_filter(0.5)]
+    )
+    data_util.create_filtered_cooler(
+        output_path, clr_test, bin_mask, chunksize=10_000_000, nproc=16
+    )
 
-    logger.debug('Start to test filter_pixels function...')
-    output_path = f"/home1/yxiao977/sc1/train_akita/test_data/test_data_util_{thres}filtered.cool"
-    bin_mask = data_util.generate_bin_mask(clr_test, [data_util.cis_total_ratio_filter(0.5)])
-    data_util.create_filtered_cooler(output_path, clr_test, bin_mask, chunksize=10_000_000, nproc=16)
-    
     clr_filtered = cooler.Cooler(output_path)
 
     # check if clr_filtered has nan cis_total ratio for bins filtered out
-    logger.debug('Start to examine the result...')
+    logger.debug("Start to examine the result...")
     coverage = cooltools.coverage(clr_test, ignore_diags=0)
     cis_total_cov = coverage[0] / coverage[1]
     bins_mask = cis_total_cov <= thres
@@ -73,9 +71,6 @@ def test_cis_total_filter(thres, clr_test=None):
     cis_total_cov_filtered_bad = cis_total_cov_filtered[bins_mask]
 
     assert np.isnan(cis_total_cov_filtered_bad).all()
-    
+
     # check if good bins has nan cis-total ratio
-    logger.info('\n\n######### Pass the test #########')
-
-
-
+    logger.info("\n\n######### Pass the test #########")
